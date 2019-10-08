ca7f5c044135861a30b67872698ea2ee2fa2a66a
sam <smuoki@healthcloud.co.ke>
2019-10-08 17:25:02 +0300

    Refactor functions

--- symmetric_encryption.py
+++ symmetric_encryption.py
@@ -24,37 +24,54 @@ def request_encryption_key():
     return encryption_key
 
 
+def request_decryption_key():
+    decryption_key = int(input("Enter an integer for decryption: "))
+    if decryption_key == 0:
+        print("Encryption key has to be greater than 1")
+        request_decryption_key()
+    return decryption_key
+
+
 def write_message():
     print("\t SYMMETRIC ENCRYPTION")
     message = str(input("Enter your message: "))
     print(message, "\n")
-
-    encryption_key = request_encryption_key()
-
     message_list = list(message)
+
+    return message_list
+
+
+def encrypt_message():
+    # Encryption process
+    message_list = write_message()
+    encryption_key = request_encryption_key()
     ordinal_values = []
     encrypted_message = []
-    decrypted_mapping = []
-    decrypted_message_list = []
-
-    # Encryption process
 
     for char in message_list:
         mapping = ord(char)
         ordinal_values.append(mapping)
         encrypted_mapping = mapping*encryption_key
         encrypted_message.append(encrypted_mapping)
+    return encrypted_message
+    # print(f"Ordinal values are: \n {ordinal_values} \n")
 
+
+def decrypt_message():
     # Decryption process
+    encrypted_message = encrypt_message()
+    decryption_key = request_decryption_key()
+    decrypted_mapping = []
+    decrypted_message_list = []
+
     for value in encrypted_message:
-        decrypted_value = int(value/encryption_key)
+        decrypted_value = int(value/decryption_key)
         decrypted_mapping.append(decrypted_value)
 
     for value in decrypted_mapping:
         char = chr(value)
         decrypted_message_list.append(char)
 
-    # print(f"Ordinal values are: \n {ordinal_values} \n")
     print(f"Encrypted message is \n {encrypted_message} \n")
     # print(f"Decrypted mapping is: \n {decrypted_mapping} \n")
     # print(f"Message list is: \n {decrypted_message_list} \n")
@@ -64,4 +81,4 @@ def write_message():
     print("\n")
 
 
-write_message()
+decrypt_message()
